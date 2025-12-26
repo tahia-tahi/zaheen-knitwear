24f560a00f7645aecd366e6591e37eb418fa1762
tahia-tahi <devpro526@gmail.com>
2025-12-26 22:04:29 +0600

    fixed variable error

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['zaheen-knitwear.vercel.app'], 
+  origin: ["https://zaheen-knitwear.vercel.app"], 
   credentials: true
 }));
 app.use(express.json());
